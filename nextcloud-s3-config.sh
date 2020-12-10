4e32534e04e04ace8af1c7de4eac386a401e4544
Matthew Cengia <mattcen@mattcen.com>
2020-12-10 15:30:41 +1100

    Make script POSIX-compliant

--- nextcloud-s3-config.sh
+++ nextcloud-s3-config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 NEXTCLOUD_CFG_SYSTEM_objectstore_class="\\OC\\Files\\ObjectStore\\S3"
 NEXTCLOUD_CFG_SYSTEM_objectstore_arguments_bucket=nextcloud
@@ -11,6 +11,7 @@ NEXTCLOUD_CFG_SYSTEM_objectstore_arguments_use__ssl=false
 NEXTCLOUD_CFG_SYSTEM_objectstore_arguments_use__path__style=true
 NEXTCLOUD_CFG_SYSTEM_objectstore_arguments_legacy__auth=true
 
+
 # Search through environment variables for NextCloud config items.  Variable
 # names should be in the form of $NEXTCLOUD_CFG_SYSTEM_var_name_here where
 # 'var_name_here' the variable name as required by `occ config:system:set`, (See
@@ -20,30 +21,29 @@ NEXTCLOUD_CFG_SYSTEM_objectstore_arguments_legacy__auth=true
 # ('_'). E.g. NEXTCLOUD_CFG_SYSTEM_foo_Bar__BAZ translates to 'foo Bar_BAZ'
 
 # Find env vars that specify NextCloud config items
-for envvar in ${!NEXTCLOUD_CFG_@};
+set | grep ^NEXTCLOUD_CFG_ | while read -r envvar
 do
   # Store value of parameter, and convert its key into a format suitable for
-  # `./occ config:system:set`, storing it in an array.
-  value=${!envvar}
-  param=${envvar#NEXTCLOUD_CFG_SYSTEM_}
-  param=${param//__/|}
-  param=${param//_/ }
-  param=${param//|/_}
-  param=($param)
+  # `./occ config:system:set`.
+  value=${envvar#*=}
+  param=${envvar%=*}
+  param=${param#NEXTCLOUD_CFG_SYSTEM_}
+  param=$(echo "$param" | sed 's/__/|/g;s/_/ /g;s/|/_/g')
 
   # Detect parameter type automatically based on its value
-  if [[ $value =~ ^[[:digit:]]*$ ]]
+  if echo "$value" | grep -q '^[[:digit:]]*$'
   then
     ptype=integer
-  elif [[ $value =~ ^[[:digit:].]*$ ]]
+  elif echo "$value" | grep -q '^[[:digit:].]*$'
   then
     ptype=float
-  elif [[ $value = true ]] || [[ $value = false ]]
+  elif echo "$value" | grep -q -e '^true$' -e '^false$'
   then
     ptype=boolean
   else
     ptype=string
   fi
 
-  docker-compose exec -T -u 33 app ./occ config:system:set "${param[@]}" --type "$ptype" --value "$value"
+  # shellcheck disable=SC2086
+  echo docker-compose exec -T -u 33 app ./occ config:system:set $param --type "$ptype" --value "$value"
 done
